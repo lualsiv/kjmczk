66200a90f229907906c33c493873f1f923ccc101
kjmczk <vollgong@gmail.com>
2021-01-23 12:42:03 +0900

    Add `SITE_URL`

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,3 +1,4 @@
+export const SITE_URL = 'https://kjmczk.dev';
 export const SITE_NAME = 'KJmczk.Dev';
 export const SITE_TITLE = 'Learn to build modern apps';
 export const GITHUB_URL = 'https://github.com/kjmczk';
